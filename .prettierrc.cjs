f091c2c4eaa62157607c72eaa90d1069ee476c30
Rein Appeldoorn <reinzor@gmail.com>
2022-09-01 12:17:14 +0200

    Project scaffold

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  importOrder: ['^@/types', '^@/store', '^@/utils', '^@/helpers', '^@/components', '^@/views', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import PrimeVue from 'primevue/config';
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import './style.css';
+
+createApp(App).use(PrimeVue).mount('#app');
